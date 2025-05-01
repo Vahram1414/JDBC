cdec90f1161e872ef8d1b2490091a45b6951006f
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2025-05-01 15:22:33 +0300

    1 May (2)

---
+++ src/main/java/repository/JdbcWriterRepositoryImpl.java
@@ -0,0 +1,4 @@
+package repository;
+
+public class JdbcWriterRepositoryImpl implements WriterRepository{
+}
---
+++ src/main/java/repository/WriterRepository.java
@@ -0,0 +1,4 @@
+package repository;
+
+public interface WriterRepository extends GenericRepository<Writer, Long>{
+}
