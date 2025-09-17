3d4214bcca10b9eff827d07b016e0f94cea0c3c5
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2025-09-17 17:24:16 +0300

    17 September

--- src/main/java/utils/JDBCUtils.java
+++ src/main/java/utils/JDBCUtils.java
@@ -1,35 +1,39 @@
 package utils;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCUtils {
-    static final String DATABASE_URL_KEY = "db.url";
+    static final String DATABASE_URL_KEY = "jdbc:mysql://127.0.0.1:3306/crud_jdbc";
 
-    static final String USER_KEY = "db.user";
-    static final String PASSWORD_KEY = "db.password";
-
-    public static Connection getConnectJDBC() {
-
-        System.out.println("Registering JDBC driver...");
+    static final String USER_KEY = "root";
+    static final String PASSWORD_KEY = "12345";
+    private static Connection connection;
+    static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        Connection connection = null;
+
+    }
+    public static Connection getConnectJDBC() throws SQLException {
+
+        System.out.println("Registering JDBC driver...");
+        if(connection == null) {
 
         try {
             connection = DriverManager.getConnection(
-                    PropertiesUtil.get(DATABASE_URL_KEY),
-                    PropertiesUtil.get(USER_KEY),
-                    PropertiesUtil.get(PASSWORD_KEY)
+                    DATABASE_URL_KEY,
+                    USER_KEY,
+                    PASSWORD_KEY
             );
             System.out.println("Creating Database connection...");
         } catch (SQLException e) {
             System.out.println("IN connection exception: " + e.getMessage());
-        }
+        }}
         return connection;
     }
 
